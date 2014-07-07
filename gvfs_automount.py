6b1d16bd13c83f14a61abd15d49e14a2b8c363b8
Laurence Gonsalves <laurence@xenomachina.com>
2014-07-07 15:50:21 -0700

    Call volume.mount_finish in on_volume_mounted.

--- gvfs_automount.py
+++ gvfs_automount.py
@@ -17,21 +17,30 @@ from pprint import pprint
 
 __author__  = 'Laurence Gonsalves <laurence@xenomachina.com>'
 
-def callback(*args, **kwargs):
+def dump_to_stdout(*args, **kwargs):
     pprint({"Args": args})
     pprint({"KWArgs": kwargs})
 
-def on_volume_added(vm, volume, _):
-    print("\nVOLUME ADDED")
+def print_volume_identifiers(volume):
     for identifier in volume.enumerate_identifiers():
         print("    %s: %r" % (identifier, volume.get_identifier(identifier)))
+
+def on_volume_added(vm, volume, _):
+    print("\nVOLUME ADDED")
+    print_volume_identifiers(volume)
     should_automount = volume.should_automount()
     print("    should_automount: %s" % should_automount)
     if should_automount:
         mount_op = Gio.MountOperation()
         # TODO: g_signal_connect (op, "ask_password", G_CALLBACK (ask_password_cb), NULL);
         # TODO: connect to the "aborted" signal? Any others?
-        volume.mount(0, mount_op, None, callback, mount_op)
+        volume.mount(0, mount_op, None, on_volume_mounted, mount_op)
+
+def on_volume_mounted(volume, async_result, mount_op):
+    if volume.mount_finish(async_result):
+        print("MOUNTED VOLUME")
+        print_volume_identifiers(volume)
+
 
 class UserError(Exception):
     def __init__(self, message):
